6261e433544bfef1aa2ad22701cf6669b39cea30
wakuma-dev <wakuma.hailu.dev@gmail.com>
2026-05-10 19:57:58 +0300

    add title description

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
-import Hero from '../components/main/home/Hero'
-import About from '../components/main/About'
-import Testimonals from '../components/main/Testimonals'
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
+import Hero from "../components/main/home/Hero";
+import About from "../components/main/About";
+import Testimonals from "../components/main/Testimonals";
 
 const Home = () => {
   return (
- <main>
-  <Hero />
-  
-  <About />
-  <Testimonals />
- </main>
+    <main>
+      <Helmet>
+        <title>Matthew Hussey | Home</title>
+        <meta
+          name="description"
+          content="Official style-inspired landing page built with React."
+        />
+      </Helmet>
 
-  )
-}
+      <Hero />
+      <About />
+      <Testimonals />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
